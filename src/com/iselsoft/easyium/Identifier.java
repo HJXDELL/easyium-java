ac806a8bad3544f534b5779c1748b28871864339
Karl Gong <karl.gong@outlook.com>
2016-03-31 21:54:02 +0800

    Remove unnecessary modifiers.

--- src/com/iselsoft/easyium/Identifier.java
+++ src/com/iselsoft/easyium/Identifier.java
@@ -1,33 +1,33 @@
 package com.iselsoft.easyium;
 
 public interface Identifier {
-    public static Identifier id = new Identifier() {
+    Identifier id = new Identifier() {
         @Override
         public String identify(Element element) {
             return "id=" + element.getAttribute("id");
         }
     };
 
-    public static Identifier className = new Identifier() {
+    Identifier className = new Identifier() {
         @Override
         public String identify(Element element) {
             return "class=" + element.getAttribute("class");
         }
     };
 
-    public static Identifier name = new Identifier() {
+    Identifier name = new Identifier() {
         @Override
         public String identify(Element element) {
             return "name=" + element.getAttribute("name");
         }
     };
 
-    public static Identifier text = new Identifier() {
+    Identifier text = new Identifier() {
         @Override
         public String identify(Element element) {
             return String.format("xpath=.//*[.='%s')]", element.getText());
         }
     };
 
-    public String identify(Element element);
+    String identify(Element element);
 }
